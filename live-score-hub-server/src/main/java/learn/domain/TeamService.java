228a23c2f6cc2fb79e810e7b96c1e935c371d6bf
BHL1994 <brien.hewanlowe@gmail.com>
2024-08-16 00:13:40 -0400

    added TeamService class and tests

---
+++ live-score-hub-server/src/main/java/learn/domain/TeamService.java
@@ -0,0 +1,24 @@
+package learn.domain;
+
+import learn.data.TeamRepository;
+import learn.models.Team;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TeamService {
+    private final TeamRepository repository;
+
+    public TeamService(TeamRepository repository) {
+        this.repository = repository;
+    }
+
+    public Team findById(int id){
+        return repository.findById(id);
+    }
+
+    public List<Team> findTeamsByCityAndName(String homeCity, String homeTeam, String awayCity, String awayTeam) {
+        return repository.findTeamsByCityAndName(homeCity, homeTeam, awayCity, awayTeam);
+    }
+}
---
+++ live-score-hub-server/src/test/java/learn/domain/TeamServiceTest.java
@@ -0,0 +1,49 @@
+package learn.domain;
+
+import learn.data.TeamRepository;
+import learn.models.League;
+import learn.models.Team;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest
+class TeamServiceTest {
+    @MockBean
+    TeamRepository repository;
+
+    @Autowired
+    TeamService service;
+
+    @Test
+    void shouldFindWhiteSoxById() {
+        Team expected = new Team(1,"Chicago White Sox", "Chicago", "White Sox", League.MLB, "CWS", "https://a.espncdn.com/i/teamlogos/mlb/500/chw.png");
+
+        when(repository.findById(1)).thenReturn(expected);
+
+        Team actual = service.findById(1);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void shouldFindTeamsByCityAndName() {
+        List<Team> expected = List.of(
+                new Team(1,"Chicago White Sox", "Chicago", "White Sox", League.MLB, "CWS", "https://a.espncdn" +
+                        ".com/i/teamlogos/mlb/500/chw.png"),
+                new Team(2,"Detroit Tigers", "Detroit", "Tigers", League.MLB, "DET", "https://a.espncdn.com/i/teamlogos/mlb/500/det.png")
+        );
+
+        when(repository.findTeamsByCityAndName(anyString(), anyString(), anyString(), anyString())).thenReturn(expected);
+
+        List<Team> actual = service.findTeamsByCityAndName("Chicago", "White Sox", "Detroit", "Tigers");
+
+        assertEquals(expected, actual);
+    }
+}
